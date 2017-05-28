4f0dc9f96efd64b107b86d3815760462bb1ef4a0
NicolasMornard <ld.nicolas.mornard@gmail.com>
2017-05-28 13:28:28 -0400

    Added timer

---
+++ GameJamBattle2017/Assets/Standard Assets/2D/Scripts/TimerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour {
+
+    public static float timeLeft = 375.0f;
+    private Text timerText;
+	// Use this for initialization
+	void Start () {
+		timerText = gameObject.GetComponentInChildren<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        timerText.text = Mathf.FloorToInt(timeLeft).ToString();
+        if (timeLeft < 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+    }
+}
