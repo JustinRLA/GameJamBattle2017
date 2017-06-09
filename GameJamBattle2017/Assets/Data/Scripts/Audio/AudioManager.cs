342e232367e60087f4699b825e01f38d1a55dc56
NikolaViel <nikola.viel@gmail.com>
2017-06-09 16:27:13 -0400

    Script wwise

---
+++ GameJamBattle2017/Assets/Data/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿
+// Auteur : Vincent Dargis
+// Edite : 30 mai 2017
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	private Transform avatarTr;
+	private Animator animator;
+	private bool crouch;
+	private bool running;
+
+	// Use this for initialization
+	void Start () 
+	{
+		animator = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//crouch = MainManager.Instance.GetAvatarController.IsCrouch;    //Get le bon path pour l'avatar.
+		//running = MainManager.Instance.GetAvatarController.IsRunning;  //Same
+
+		if(crouch)
+		{
+			AkSoundEngine.SetState ("Speed_State", "Crouched");
+		}
+		if(running)
+		{
+			AkSoundEngine.SetState ("Speed_State", "Running");
+		}
+		if(!running && !crouch)
+		{
+			AkSoundEngine.SetState ("Speed_State", "Walking");	
+		}
+
+	}
+
+	void PlaySound(string input)
+	{
+		AkSoundEngine.PostEvent (input, gameObject);
+	}
+}
---
+++ GameJamBattle2017/Assets/Data/Scripts/Audio/MusicController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Scene scene = SceneManager.GetActiveScene ();
+		string myScene = scene.name;
+
+		switch (myScene) 
+		{
+		case "scene01_LA":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel01", gameObject);
+			break;
+		
+		case "scene02_LA":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel02", gameObject);
+			break;
+
+		case "scene03_LA":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel03", gameObject);
+			break;
+
+		case "scene04_LA":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel04", gameObject);
+			break;
+
+		case "scene05_LA":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel05", gameObject);
+			break;
+
+		case "scene06_LA":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel06", gameObject);
+			break;
+
+		case "scene07_LA":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel07", gameObject);
+			break;
+
+		case "scene08_LA":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel08", gameObject);
+			break;
+
+		case "scene09_LA":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel09", gameObject);
+			break;
+
+		case "scene01_LD":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel01", gameObject);
+			break;
+
+		case "scene02_LD":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel02", gameObject);
+			break;
+
+		case "scene03_LD":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel03", gameObject);
+			break;
+
+		case "scene04_LD":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel04", gameObject);
+			break;
+
+		case "scene05_LD":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel05", gameObject);
+			break;
+
+		case "scene06_LD":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel06", gameObject);
+			break;
+
+		case "scene07_LD":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel07", gameObject);
+			break;
+
+		case "scene08_LD":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel08", gameObject);
+			break;
+
+		case "scene09_LD":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel09", gameObject);
+			break;
+
+		case "Vincent_Gym":
+			AkSoundEngine.PostEvent ("Play_AmbianceLevel09", gameObject);
+			break;
+
+		default:
+			break;
+		}
+	
+			
+	}
+
+}
