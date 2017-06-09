f51c2a68b74fe749a2e148ce052e18cbff45176d
NikolaViel <nikola.viel@gmail.com>
2017-06-09 15:23:26 -0400

    Soundbank

---
+++ GameJamBattle2017/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,67 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMB_LEVEL_01 = 2894026586U;
+        static const AkUniqueID DENIS_ELECTRIC_01 = 4194456506U;
+        static const AkUniqueID DENIS_JUMP = 2741272089U;
+        static const AkUniqueID DENIS_LAND_01 = 1122608996U;
+        static const AkUniqueID DENIS_WALK = 2579127662U;
+        static const AkUniqueID LIGHT_CONNECTED = 3359155179U;
+        static const AkUniqueID MUS_LEVEL_01 = 515498461U;
+        static const AkUniqueID MUS_MENU_01 = 4234731666U;
+        static const AkUniqueID PIGEON_CHATTER_01 = 3048554965U;
+        static const AkUniqueID PIGEON_FLY_01 = 120299665U;
+        static const AkUniqueID PLATFORM_LOOP_01 = 130275037U;
+        static const AkUniqueID POWER_LOOP_01 = 733923149U;
+        static const AkUniqueID UI_CANCEL_01 = 3356300706U;
+        static const AkUniqueID UI_HOVER_01 = 347377382U;
+        static const AkUniqueID UI_SELECT_01 = 591949760U;
+        static const AkUniqueID WIRE_BACK_01 = 2254469782U;
+        static const AkUniqueID WIRE_REWIND_01 = 106057874U;
+        static const AkUniqueID WIRE_STRETCH_01 = 2513369922U;
+        static const AkUniqueID WIRE_STUCK = 3051451975U;
+        static const AkUniqueID WIRE_TAKE_01 = 3184674562U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace WALK
+        {
+            static const AkUniqueID GROUP = 2108779966U;
+
+            namespace STATE
+            {
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace STATE
+        } // namespace WALK
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ALL = 1100754030U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
